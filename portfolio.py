6a0460dfc1ec008ad05301e304b334ed0680672d
Anthony Lieu <al6907@g.rit.edu>
2024-07-21 22:21:23 -0400

    add getters for Portfolio attributes and allow the Portfolio to track the price each stock was bought at

--- portfolio.py
+++ portfolio.py
@@ -1,26 +1,47 @@
 from stock import *
 
 class Portfolio:
-    __slots__ = ["stocks", "total_assets", "available_funds", "total_gains", "total_losses"]
+    __slots__ = ["stocks", "principal_investment", "total_assets", "available_funds", "returns"]
 
-    def __init__(self, available_funds: float):
-        self.stocks = dict(Stock)
+    def __init__(self, principal_investment: float):
+        self.stocks = dict[Stock, (float, int)] # stock : (price bought at, quantity)
+        self.principal_investment = principal_investment
         self.total_assets = 0.0
-        self.available_funds = available_funds
+        self.available_funds = principal_investment
         self.total_assets = 0.0
         self.total_losses = 0.0
 
-    def buy_stock(self, stock: Stock) -> None:
-        self.available_funds -= stock.get_current_price
+    def buy_stocks(self, stock: Stock, quantitiy: int) -> None:
+        current_price = stock.get_current_price
+        self.available_funds -= current_price
 
-        if stock in self.stocks.keys:
-            self.stocks[stock] += 1
+        if stock in self.stocks.keys and self.stocks[stock][0] == current_price:
+            self.stocks[stock][1] += quantitiy
         else:
-            self.stocks[stock] = 1
+            self.stocks[stock][0] = current_price
+            self.stocks[stock][1] = quantitiy
 
-    def sell_stock(self, stock: Stock) -> None:
-        self.available_funds += stock.get_current_price
-        self.stocks[stock] -= 1
+    def sell_stocks(self, stock: Stock, quantity: int) -> None:
+        self.available_funds += stock.get_current_price * quantity
+        self.stocks[stock][1] -= quantity
 
-        if self.stocks[stock] == 0:
+        if self.stocks[stock][1] == 0:
             self.stocks.pop(stock)
+
+    def update_returns(self) -> None:
+        principal = self.principal_investment
+
+        for stock in self.stocks:
+            stock.get_current_price * self.stocks[stock]
+
+    def get_stocks(self) -> dict(Stock): # type: ignore
+        return self.stocks
+    
+    def get_total_assets(self) -> float:
+        return self.total_assets
+    
+    def get_available_funds(self) -> float:
+        return self.available_funds
+    
+    def get_net_change(self) -> float:
+        return self.net_change
